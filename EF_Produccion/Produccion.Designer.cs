f1462421f5ebd3ca591369793b20e63739883eda
hernandl <luis.hernandez@gmx.com.mx>
2018-12-26 09:13:29 -0600

    Ticket 14866 (longitud insuficiente en [txt_certificado]) resuelto. Lote de diciembre 2018 generado.

--- EF_Produccion/Produccion.Designer.cs
+++ EF_Produccion/Produccion.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\hernandl\documents\visual studio 2015\Projects\CertificadoLoteDecenalMVC\EF_Produccion\Produccion.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\hernandl\Documents\Visual Studio 2015\Projects\CertificadoLoteDecenalMVC\EF_Produccion\Produccion.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
